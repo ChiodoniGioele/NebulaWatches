4079e5dd844c9843518bfcbdcdb18049c70fb0b9
ChiodoniGioele <gioele.chiodoni@samtrevano.ch>
2024-01-26 09:48:13 +0100

    logn

---
+++ 5_Applicativo/nebulaWatchesAPI/src/test/java/ch/nebulaWatches/nebulaWatchesAPI/NebulaWatchesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.nebulaWatches.nebulaWatchesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NebulaWatchesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
