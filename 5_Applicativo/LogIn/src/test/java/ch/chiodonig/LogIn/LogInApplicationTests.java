da960695f7048634623b22edebe961c59352371e
ChiodoniGioele <gioele.chiodoni@samtrevano.ch>
2024-02-02 10:12:18 +0100

    google

---
+++ 5_Applicativo/LogIn/src/test/java/ch/chiodonig/LogIn/LogInApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.chiodonig.LogIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
