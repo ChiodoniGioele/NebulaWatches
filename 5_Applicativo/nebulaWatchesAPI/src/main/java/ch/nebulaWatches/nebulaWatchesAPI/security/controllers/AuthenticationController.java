fcbfa755a73988aeadc142ae4a50d2a661d472dc
Tom Schillerwein <96230306+SchillerweinTom@users.noreply.github.com>
2024-02-09 14:32:56 +0100

    Merge Gui to Dev

--- 5_Applicativo/nebulaWatchesAPI/src/main/java/ch/nebulaWatches/nebulaWatchesAPI/security/controllers/AuthenticationController.java
+++ 5_Applicativo/nebulaWatchesAPI/src/main/java/ch/nebulaWatches/nebulaWatchesAPI/security/controllers/AuthenticationController.java
@@ -6,14 +6,14 @@ import ch.nebulaWatches.nebulaWatchesAPI.security.models.RegisterRequest;
 import ch.nebulaWatches.nebulaWatchesAPI.security.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
+<<<<<<< HEAD
 @RequestMapping("")
 =======
+@RequestMapping("/auth")
 >>>>>>> origin/watches-gui
 @RequiredArgsConstructor
 public class AuthenticationController {
