4b39bc619f3ae5220d0576a93fcfede67e4c882c
CiobanuAlexandruAndrei <alexandru.ciobanu@samtrevano.ch>
2024-02-09 13:24:40 +0100

    Home iniziata

---
+++ 5_Applicativo/client/src/components/Sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex flex-col"> <!-- Ensure the sidebar is a flex container with column direction -->
+      <div class="space-y-4 py-4 flex-1"> <!-- Use flex-1 to allow the sidebar to take full height -->
+        <div class="px-3 py-2">
+          <div class="space-y-1">
+            <Button variant="ghost" class="w-full justify-start">
+              <span class="text-gray-800">Watches</span>
+            </Button>
+            <Button variant="ghost" class="w-full justify-start">
+                <span class="text-gray-800">Inventory</span>
+            </Button>
+            <Button variant="ghost" class="w-full justify-start">
+                <span class="text-gray-800">Clients</span>
+            </Button>
+            <Button variant="ghost" class="w-full justify-start">
+                <span class="text-gray-800">Team</span>              
+            </Button>
+            <Button variant="ghost" class="w-full justify-start">
+                <span class="text-gray-800">Comunication</span>              
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { Button } from '../components/ui/button'
+  </script>
+  
--- 5_Applicativo/client/src/views/Home.vue
+++ 5_Applicativo/client/src/views/Home.vue
@@ -1,3 +1,66 @@
 <template>
-    <h1>Home</h1>
+
+    <div class="grid lg:grid-cols-5 min-h-screen"> 
+        <Sidebar class="hidden lg:block" />
+        <div class="col-span-3 lg:col-span-4 lg:border-l flex flex-col"> 
+            <div class="px-4 py-6 lg:px-8"> 
+
+                <div class="flex w-full items-center gap-1.5">
+                    <Input id="email" type="text" placeholder="Search..." />
+                    <Button type="submit" variant="outline"> Search </Button>
+                </div>
+
+                <div class="mt-3">
+                    <Popover>
+                        <PopoverTrigger>
+                            <Button variant="outline"> <span class="font-normal">Brands (10)</span> </Button>
+                        </PopoverTrigger>
+                        <PopoverContent class="w-auto">
+                            <div class="flex items-center space-x-2">
+                                <Checkbox id="omegaselected" />
+                                <label
+                                for="omegaselected"
+                                class="text-sm font-light leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                >
+                                Omega
+                                </label>
+                            </div>
+                            <div class="flex items-center space-x-2 mt-3">
+                                <Checkbox id="rolexselected" />
+                                <label
+                                for="rolexselected"
+                                class="text-sm font-light leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                >
+                                Rolex
+                                </label>
+                            </div>
+
+
+                        </PopoverContent>
+                    </Popover>
+                </div>
+                
+
+            </div>
+        </div>
+
+        
+    </div>
+    
+ 
 </template>
+  
+<script setup lang="ts">
+import Sidebar from '../components/Sidebar.vue'
+import { Input } from '../components/ui/input'
+import { Button } from '../components//ui/button'
+import { Checkbox } from '../components/ui/checkbox'
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+
+
+</script>
