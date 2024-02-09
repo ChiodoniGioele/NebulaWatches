7708df7cc99231ae55f3ccb4d5fa535be6d2c4a2
SchillerweinTom <tom.schillerwein@samtrevano.ch>
2024-02-09 15:21:38 +0100

    Update for server

--- 5_Applicativo/nebulaWatchesAPI/src/main/java/ch/nebulaWatches/nebulaWatchesAPI/security/controllers/AuthenticationController.java
+++ 5_Applicativo/nebulaWatchesAPI/src/main/java/ch/nebulaWatches/nebulaWatchesAPI/security/controllers/AuthenticationController.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
