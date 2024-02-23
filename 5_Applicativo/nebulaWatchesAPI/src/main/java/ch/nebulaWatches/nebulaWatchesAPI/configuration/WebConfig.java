684bbd2ccf6982e7d9a3520922b852ffaa3dd247
ChiodoniGioele <gioele.chiodoni@samtrevano.ch>
2024-02-23 14:33:26 +0100

    fes

--- 5_Applicativo/nebulaWatchesAPI/src/main/java/ch/nebulaWatches/nebulaWatchesAPI/configuration/WebConfig.java
+++ 5_Applicativo/nebulaWatchesAPI/src/main/java/ch/nebulaWatches/nebulaWatchesAPI/configuration/WebConfig.java
@@ -10,5 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("forward:/index.html");
+     // registry.addViewController("/login").setViewName("forward:/");
     }
 }
