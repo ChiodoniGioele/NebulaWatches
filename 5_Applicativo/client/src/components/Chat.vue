683ef70f1541b1fa9519254d186488c9a20b389a
Tom Schillerwein <tom.schillerwein@samtrevano.ch>
2024-05-03 14:58:59 +0200

    Informations

--- 5_Applicativo/client/src/components/Chat.vue
+++ 5_Applicativo/client/src/components/Chat.vue
@@ -3,13 +3,13 @@
         <div class="bg-white rounded-lg shadow-lg w-[32rem] border-2 border-stone-900 z-50" style="z-index: 9999;">
             <div class="flex justify-between items-center p-4 border-b border-gray-200">
                 <div>
-                    <div class="text-lg font-semibold flex gap-4 items-center"> 
+                    <div class="text-lg font-semibold flex gap-4 items-center">
                         <img class="h-[20px] max-w-[40px]" src="@/assets/icons/bot_icon.png" alt="bot icon" />
                         <h3>Gio</h3>
-                        
+
                     </div>
                 </div>
-                
+
                 <button @click="toggleChat" class="text-gray-500 hover:text-gray-600 focus:outline-none">
                     <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
@@ -31,16 +31,38 @@
                 </div>
             </div>
             <div class="p-4 border-t border-gray-200 flex gap-1">
-                <Button variant="ghost"> 
-                    <img class="" height="20px" width="20px" src="@/assets/icons/info_icon.png" alt="info" />
-                </Button>
+                <Dialog>
+                    <DialogTrigger>
+                        <Button variant="ghost">
+                            <img class="" height="40px" width="40px" src="@/assets/icons/info_icon.png" alt="info" />
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>Info</DialogTitle>
+                        </DialogHeader>
+
+                        <p>
+
+                            By engaging with this bot, you explicitly agree to the disclosure of your data to OpenAI and
+                            Gio, referred to henceforth as the "Entities". This agreement encompasses all data,
+                            inclusive of personal information, usage patterns, and content inputs, generated or provided
+                            during interactions with the bot. The Entities reserve the right to utilize this data for
+                            diverse purposes, encompassing but not limited to research, development, and enhancement of
+                            their respective offerings. It is understood that the Entities will adhere to pertinent data
+                            protection laws and regulations, and undertake reasonable measures to maintain the
+                            confidentiality and integrity of the shared data.
+                        </p>
+                    </DialogContent>
+                </Dialog>
+
                 <Input v-model="inputMessage" @keyup.enter="sendMessage" type="text"
                     class="mx-1 w-full px-2 py-1 border border-gray-300 rounded-lg focus:outline-none"
                     placeholder="Type a message..." />
-                <Button @click="sendMessage"> 
+                <Button @click="sendMessage">
                     <img class="" height="20px" width="20px" src="@/assets/icons/send_white_icon.png" alt="send" />
                 </Button>
-                <Button variant="destructive"> 
+                <Button variant="destructive">
                     <img class="" height="20px" width="20px" src="@/assets/icons/trash_icon.png" alt="delete" />
                 </Button>
             </div>
@@ -60,10 +82,20 @@ import axios from 'axios';
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
 } from '@/components/ui/hover-card'
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog'
 
 
 const isOpen = ref(false);
